0ec6c079f809770f4bb0095542c046f488687863
ThinhNguyen <86031845+milesnguyen@users.noreply.github.com>
2022-11-07 10:01:52 +0700

    portfolio

--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -3,17 +3,31 @@ import DefaultLayout from '../../Layouts/DefaultLayout';
 import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
 import styles from './Home.module.scss';
+import { useState } from 'react';
 
 const cx = classNames.bind(styles);
 
 function Home() {
+    const [pointY, setPointY] = useState([0]);
+    const [pointX, setPointX] = useState([0]);
+
+    const styles = {
+        transform: `translate(${pointX}px, ${pointY}px)`,
+    };
+    const handleMouse = (e) => {
+        setPointY(e.pageY / 8);
+        setPointX(e.pageX / 8);
+    };
+
     return (
         <div>
             <DefaultLayout />
             <div className={cx('wrapper')}>
                 <div className={cx('inner')}>
                     <div className={cx('head')}>
-                        <span className={cx('gradient')}>Thinh Nguyen</span>
+                        <span onMouseMoveCapture={handleMouse} style={styles} className={cx('gradient')}>
+                            Thinh Nguyen
+                        </span>
                     </div>
                     <div className={cx('job')}>
                         <span>Front</span>
--- src/Page/Portfolio/Portfolio.js
+++ src/Page/Portfolio/Portfolio.js
@@ -1,11 +1,64 @@
+import { faCode } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
 import DefaultLayout from '../../Layouts/DefaultLayout';
+import styles from './Portfolio.module.scss';
+
+const cx = classNames.bind(styles);
 
 function Portfolio() {
     return (
         <>
-            <DefaultLayout /> <h1>Portfolio</h1>
+            <DefaultLayout />{' '}
+            <div className={cx('wrapper')}>
+                <div className={cx('inner')}>
+                    <div className={cx('content')}>
+                        <img
+                            src="https://img.upanh.tv/2022/11/07/image93e55b5f940a558f.png"
+                            alt="image93e55b5f940a558f.png"
+                            border="0"
+                        />
+                        <div className={cx('item')}>
+                            <span>
+                                <p>
+                                    <FontAwesomeIcon icon={faCode} />
+                                </p>
+                                ReactJS, SCSS
+                            </span>
+                            <h1>TikTok Clone</h1>
+                            <button>
+                                <a href="https://tiktok-ui-miles.vercel.app/" target="_blank">
+                                    View More
+                                </a>
+                            </button>
+                        </div>
+                    </div>
+                    <div className={cx('content')}>
+                        <img
+                            src="https://img.upanh.tv/2022/11/07/image66717715e6a6218e.png"
+                            alt="image66717715e6a6218e.png"
+                            border="0"
+                        />
+                        <div className={cx('item')}>
+                            <span>
+                                <p>
+                                    <FontAwesomeIcon icon={faCode} />
+                                </p>
+                                HTML, CSS, JavaScript
+                            </span>
+                            <h1>Music App</h1>
+                            <button>
+                                <a href="https://milesnguyen.github.io/app-music/" target="_blank">
+                                    View More
+                                </a>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
 
 export default Portfolio;
+<img src="https://img.upanh.tv/2022/11/07/image93e55b5f940a558f.png" alt="image93e55b5f940a558f.png" border="0" />;
